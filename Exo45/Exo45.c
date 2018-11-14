1d083e613ad07cd24a35a55cdf6e8fbd5616ed59
etemesgen <44799499+etemesgen@users.noreply.github.com>
2018-11-14 22:57:25 +0100

    Add files via upload

---
+++ Exo45/Exo45.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "stdlib.h"
+
+int main(void)
+{
+	int joueur = 0;
+	
+	for( int a = 1 ; a = 2; a = 3)
+	{
+	
+	while(a != 1){
+	
+	if(a %2 == 0 )
+	{
+		joueur = joueur + 2;
+	}
+   
+		else 
+		{
+			joueur = joueur + 1;
+		}
+	
+	
+	if (a == 3)
+	{
+		joueur = joueur*2;
+	}
+	else 
+	{ 
+		joueur = joueur + 1;
+	}	
+		
+    if (a == 5)
+	{
+		joueur = joueur - 2;
+	}
+	else
+	{
+		joueur = 1;
+	}
+	
+	return 0;
+	}
+  }
+}
+		
+	
+		
+		
+		
+			
+			
+		
+	
