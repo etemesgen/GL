cfc9d79ff0b8774dfcfa0a4714cd658e3d98d2e7
edomiyas temesgen <edomiyas.temesgen@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:39:33 +0100

    jai demarre lexo

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
-int main(void)
+
+int main (void)
 {
-    return 0;
+	unsigned char combinaison_g[6] = {1,5,10,40,28,34};
+	unsigned char combinaison_j[6] = {48,39,12,17,8,2};
+	
+
+	
+	
+	
+	
+	return 0;
 }
+
