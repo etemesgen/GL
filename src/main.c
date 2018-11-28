b4e622e33a355f303d96374d8f91aa79356bc3f3
Edomyas Temesgen <edomiyas.temesgen@u-psud.fr>
2018-11-29 00:02:18 +0100

    jai fini exo 58

--- src/main.c
+++ src/main.c
@@ -1,16 +1,45 @@
 #include "my_header.h"
+#include "TP_lib_suite.h"
 
 
 int main (void)
 {
-	unsigned char combinaison_g[6] = {1,5,10,40,28,34};
-	unsigned char combinaison_j[6] = {48,39,12,17,8,2};
+	unsigned char combinaison_g[6];
+	unsigned char combinaison_j[6] = {48,39,5,17,8,2};
+	unsigned char i;
+	unsigned char e;
+	unsigned char nombres_valeurs = 0;
+	unsigned char c;
+	unsigned char meilleur_score = 0;
 	
-
+	for (c=0; c<100; c++)
+	{
+	    initialiserTirage();
 	
+	for (e=0; e<6; e++)
+	{
+		combinaison_g[e] = tirerNumero();
+	}
 	
-	
+	    for (i=0; i<6; i++)
+		{
+			for (e=0; e<6; e++)
+			{
+				if(combinaison_g[i] == combinaison_j[e])
+			    {
+				    nombres_valeurs = nombres_valeurs + 1;
+	            }
+			}
+			
+       }
+	   
+	   if(meilleur_score < nombres_valeurs)
+				{
+					meilleur_score = nombres_valeurs;
+				}
+				
+	   			nombres_valeurs = 0;
+	}
 	
 	return 0;
 }
-
