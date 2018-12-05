ee79290f86e5720ad760eac66ae7d23035a73dfc
Edomyas Temesgen <edomiyas.temesgen@u-psud.fr>
2018-12-05 20:40:47 +0100

    Exo79 fait

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
+#include <userint.h>
+#include <ansi_c.h>
 
 int main(void)
 {
-    return 0;
+	char chaine[100] = "je m'appelle Lea Dupont et j'ai 19 ans";
+	scanf("%s", chaine);
+
+	
+	unsigned long int phrase[100]; 
+	char prenom [100];
+	unsigned char age[100];
+	
+	sscanf(chaine,"%lu %lu %lu %s %s %lu %lu %lu %lu %u %*s", phrase, prenom, age); 
+	MessagePopup(chaine, "je m'appelle Lea Dupont et j'ai 19 ans");
+	
+	
+	
+	return 0;
 }
+
