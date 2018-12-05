ec9f478e74b852be601885594f856e9efe6f30bc
Edomyas Temesgen <edomiyas.temesgen@u-psud.fr>
2018-12-05 22:24:00 +0100

    Exo82 essaye

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <ansi_c.h>
 
-#include "my_header.h"
 
 int main(void)
 {
-    return 0;
+	char ligne1[80] =  "nom: triangle_A" ; 
+	char ligne2[80] =  "nb points: 3" ; 
+	char ligne3[80] =  "cordonnees:   10 20 30 20 30 40 43 53 35 65" ; 
+	
+	
+	char nom_fig[80];
+	char nb_points[80];
+	char coordonnees[80];
+	
+	scanf(ligne1, "%s" , nom_fig);
+	scanf(ligne2, "%s" , nb_points);
+	scanf(ligne3, "%s", coordonnees);
+	
+	sprintf(ligne1, "%s" , nom_fig);
+	sprintf(ligne2, "%s" , nb_points);
+	sprintf(ligne3, "%s" , coordonnees);
+	
+
+return 0;
 }
